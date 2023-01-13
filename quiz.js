ad77e04565216e75e61adae119e30ce5674906fe
cysnova <cysnova@gmail.com>
2023-01-12 21:15:28 -0800

    first commit

---
+++ quiz.js
@@ -0,0 +1,108 @@
+// at the lesson
+
+// const arrowFunction = () => {
+    
+// }
+
+// const arrowFunction2 = () => "Hello";
+
+// console.log(arrowFunction2());
+
+// const arrowFunction3 = (age) => `I am ${age} years old`;
+
+// console.log(arrowFunction3(31));
+
+// const greeting = name => `Hello ${name};`
+
+// console.log(greeting("ada"));
+
+// const code = "ABCDE";
+
+// console.log(code.startsWith("ABC"));
+
+// console.log(code.startsWith("CDE",2));
+
+// const greeting = "Hello!"
+
+// console.log(greeting.repeat(10000));
+// const age = 10;
+// console.log(typeof age);
+
+// const hasKids = true;
+// console.log(hasKids);
+
+// const car = null;
+// console.log(typeof car);
+
+// let test;
+// console.log(typeof test);
+
+// const today = new Date();
+// console.log(today);
+
+// let val;
+
+// val = String(5);
+// console.log(val);
+
+// console.log(typeof val);
+// console.log(val.length);
+
+
+// let val;
+// val = Math.floor(Math.random()*20+1);
+// console.log(val);
+
+// const firstName = "Nova";
+// const lastName = "Sin";
+// const age = 8;
+// let user = firstName + " " + lastName;
+
+
+// user = "Ada ";
+// user += "John";
+
+// user = 'Hello, my name is ' + firstName + ' and i am ' + age+ 'years old';
+
+// user = firstName.concat(" ",lastName);
+
+// user = firstName.toLowerCase(firstName);
+
+// user = firstName.lastIndexOf('o');
+
+// console.log(user);
+
+// 1. doesn't work
+const firstName = "John";
+const age = 40;
+const city = "New York"
+const job = "Web developer"
+let html;
+html = `
+    <ul>
+    <li> Name : ${firstName} </li>
+    <li> Age : ${age} </li>
+    <li> City : ${city} </li>
+    <li> Job : ${job}</li>
+    </ul>
+`;
+
+document.body.innerHTML = html;
+
+
+// let val;
+    
+// const numbers = [1,2,3];
+// const num1 = [6,7,4, 4,0,102];
+// val = num1.concat(numbers);
+
+// let sortArray;
+
+// sortArray = val.sort(function(x,y){
+//     return y - x;
+// });
+
+// console.log(sortArray);
+
+
+
